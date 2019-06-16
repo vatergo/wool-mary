3a5bc08b0b4d13d43f84712568925477f4e7b60f
vatergo <vatergo@mail.ru>
2019-06-16 19:48:15 +0500

    Updates

--- precache-manifest.38dc25c0c9457c088514b071052c2198.js
+++ precache-manifest.38dc25c0c9457c088514b071052c2198.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "98b0923b749cbeabb9964104b3dbe6d8",
+    "revision": "d898cd56896841aedb6f385506a23dbe",
     "url": "/wool-mary/index.html"
   },
   {
-    "revision": "c773c7a9157caec98b8a",
-    "url": "/wool-mary/static/css/main.57ff0e35.chunk.css"
+    "revision": "f07d089570b01d7a9260",
+    "url": "/wool-mary/static/css/main.3d01442e.chunk.css"
   },
   {
     "revision": "c763d0a5a6b924201aa4",
     "url": "/wool-mary/static/js/2.c306511a.chunk.js"
   },
   {
-    "revision": "c773c7a9157caec98b8a",
+    "revision": "f07d089570b01d7a9260",
     "url": "/wool-mary/static/js/main.980d67d7.chunk.js"
   },
   {
